53be339d265fe3f28a3675215082deedd13d74d0
BayashiPascal <bayashipascal@gmail.com>
2021-06-08 16:59:30 +0900

    Add unit test, wip

--- main.c
+++ main.c
@@ -9,64 +9,24 @@ struct Dummy {
 
 };
 
+// GSet of pointer to Dummy struct
 DefineGSet(Dummy, struct Dummy*)
+
+#define sizeArr 10
+char arrChar[sizeArr] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'};
+
+#define Test(Name, Type)                                                     \
+  do {                                                                       \
+    GSet ## Name* setA = GSet ## Name ## Alloc();                            \
+    GSet ## Name* setB = GSet ## Name ## FromArr(arr ## Name, sizeArr);      \
+    GSetFree(&setA);                                                         \
+    GSetFree(&setB);                                                         \
+  } while(false)
 
 // Main function
 int main() {
 
-  // Init the random generator
-  srand(0);
-
-  // Example of GSet with element of type int
-
-  // Create the GSet
-  GSetInt* setInt = GSetIntAlloc();
-  GSetUInt* setUInt = GSetUIntAlloc();
-  GSetStr* setStr = GSetStrAlloc();
-  GSetDummy* setDummy = GSetDummyAlloc();
-
-  // Push data
-  int aInt = 0;
-  GSetPush(setInt, aInt);
-  int aUInt = 0;
-  GSetPush(setUInt, aUInt);
-  char* aStr = NULL;
-  GSetPush(setStr, aStr);
-  struct Dummy* aDummy = NULL;
-  GSetPush(setDummy, aDummy);
-
-  // Pop data
-  aInt = GSetPop(setInt);
-  aUInt = GSetPop(setUInt);
-  aStr = GSetPop(setStr);
-  aDummy = GSetPop(setDummy);
-
-  // Should not compile without warning
-  // ok GSetPush(setStr, aDummy);
-  // ok aStr = GSetPop(setDummy);
-
-  // Iterator
-  GSetIterInt* iterInt =
-    GSetIterIntAlloc(
-      setInt,
-      GSetIterForward);
-  GSetIterReset(iterInt);
-  //aInt = GSetIterGet(iterInt);
-  GSetIterDummy* iterDummy =
-    GSetIterDummyAlloc(
-      setDummy,
-      GSetIterBackward);
-  GSetIterReset(iterDummy);
-  //aDummy = GSetIterGet(iterDummy);
-
-  // Free memory
-  GSetFree(&setInt);
-  GSetFree(&setUInt);
-  GSetFree(&setStr);
-  GSetFree(&setDummy);
-
-  GSetIterFree(&iterInt);
-  GSetIterFree(&iterDummy);
+  Test(Char, char);
 
   // Return the sucess code
   return EXIT_SUCCESS;
